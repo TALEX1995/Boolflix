c27952bbe3dff763c515347a709d7b0d9286165c
TALEX1995 <tanase.alex@libero.it>
2023-07-05 17:39:37 +0200

    Added flag on main card

--- src/components/CreateCard.vue
+++ src/components/CreateCard.vue
@@ -8,14 +8,21 @@ export default {
 
 <template>
     <ul>
-        <li v-for="item in listItem">
+        <li v-for="item in listItem" key="item">
             <span>Titolo originale: {{ item.originalTitle }}</span> <br>
             <span>Titolo in Italiano: {{ item.itTitle }}</span> <br>
-            <span>Lingua: {{ item.originalLanguage }}</span> <br>
+            <span v-if="item.originalLanguage === 'en' || item.originalLanguage === 'it'"><img
+                    :src="`./src/assets/img/${item.originalLanguage}.png`" :alt="`flag ${item.originalLanguage}`"></span>
+            <span v-else>Lingua: {{ item.originalLanguage }}</span> <br>
             <span>Voto: {{ item.vote }}</span> <br>
         </li>
     </ul>
 </template>
 
 
-<style></style>
+<style scoped>
+img {
+    max-width: 40px;
+    height: auto;
+}
+</style>
