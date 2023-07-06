d8daf854850bd02794df4ef3b16ca16374d6d42b
TALEX1995 <tanase.alex@libero.it>
2023-07-06 16:39:44 +0200

    Added star icon from font awesome

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -25,6 +25,8 @@ export default {
 </script>
 
 <template>
+    <font-awesome-icon :icon="['fas', 'star']" />
+    <font-awesome-icon :icon="['far', 'star']" />
     <SearchBar :labelInput="'Inserisci il titolo del film o della serie tv'" @form-submit="getTitleFilter" />
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+/* import specific icons */
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faStar, faStarRegular)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
+
